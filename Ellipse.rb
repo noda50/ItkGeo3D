af3aef8028a0161e392443273aedb8e071730286
Itsuki Noda <i.noda@aist.go.jp>
2024-11-13 20:26:10 +0900

    class Ellipse: distance to Point, LineSegment, Ellipse.

--- Ellipse.rb
+++ Ellipse.rb
@@ -169,6 +169,193 @@ class Ellipse < GeoObject
     return _ring ;
   end
 
+  #--////////////////////////////////////////////////////////////
+  #--------------------------------------------------------------
+  #++
+  ## 垂線の足(近い方)のある位置の @axisMajor からの角度(単位：radian)を求める。
+  ## ただし、@axisMajor と @axisMinor が同じ長さで直交していると仮定。
+  ## （つまり楕円でなく、円）
+  ## _point_:: 垂線を下ろす点。
+  ## *return*:: 角度 in radian.
+  def footPointAngleFrom(_point)
+    _axisMajorLine = LineSegment.new(@center, @center + @axisMajor) ;
+    _axisMinorLine = LineSegment.new(@center, @center + @axisMinor) ;
+
+    _fracMajor = _axisMajorLine.footPointFractionFrom(_point, true) ;
+    _fracMinor = _axisMinorLine.footPointFractionFrom(_point, true) ;
+
+    _angle = Math.atan2(_fracMinor, _fracMajor) ;
+
+    return _angle ;
+  end
+
+  #------------------------------------------
+  #++
+  ## 円弧上の角度
+  ## _point_:: 円弧状の点。
+  ## *return*:: 角度
+  def arcPointAngle(_point)
+    return footPointAngleFrom(_point) ;
+  end
+  
+  #------------------------------------------
+  #++
+  ## 垂線の足の点
+  ## _point_:: 垂線を下ろす点。
+  ## *return*:: 垂線の足の位置
+  def footPointFrom(_point)
+    _angle = self.footPointAngleFrom(_point) ;
+    _footPoint = self.arcPoint(_angle) ;
+
+    return _footPoint ;
+  end
+
+  #------------------------------------------
+  #++
+  ## 線分からの垂線の足の点
+  ## _line_:: 垂線を下ろす線分。
+  ## _extendP_:: 線分を延長するかどうか？
+  ## *return*:: [円弧上の点、線上の点]
+  def closestPointPairFromLineSegment(_line, _extendP = false)
+    _arcPointPre = nil ;
+    _arcPoint = nil ;
+    _linePointPre = nil ;
+    _linePoint = _line.u ;
+    until(_linePointPre &&
+          isAlmostZero(_linePointPre.distanceToPoint(_linePoint)))
+      _arcPointPre = _arcPoint ;
+      _arcPoint = self.footPointFrom(_linePoint) ;
+      
+      _linePointPre = _linePoint ;
+      _linePoint = _line.footPointFrom(_arcPoint, _extendP) ;
+    end
+
+    return [_arcPoint, _linePoint] ;
+  end
+
+  #------------------------------------------
+  #++
+  ## 円弧からの垂線の足の点
+  ## _other_:: 垂線を下ろす円。
+  ## *return*:: [self上の点、other上の点]
+  def closestPointPairFromEllipse(_other)
+    _selfPointPre = nil ;
+    _selfPoint = nil
+    _otherPointPre = nil ;
+    _otherPoint = _other.arcPoint(0) ;
+    until(_otherPointPre &&
+          isAlmostZero(_otherPointPre.distanceToPoint(_otherPoint)))
+      _selfPointPre = _selfPoint ;
+      _selfPoint = self.footPointFrom(_otherPoint) ;
+      
+      _otherPointPre = _otherPoint ;
+      _otherPoint = _other.footPointFrom(_selfPoint) ;
+    end
+
+    return [_selfPoint, _otherPoint] ;
+  end
+
+  #--------------------------------------------------------------
+  #++
+  ## 点までの距離関係情報
+  ## _point_:: 対象となる Point
+  ## *return*:: [_distance_, _footPoint_, _angle_]
+  def distanceInfoToPoint(_point)
+    _angle = self.footPointAngleFrom(_point) ;
+    _footPoint = self.arcPoint(_angle) ;
+    _distance = _footPoint.distanceToPoint(_point) ;
+
+    return [_distance, _footPoint, _angle] ;
+  end
+  
+  #----------------------
+  #++
+  ## 点までの距離
+  ## _point_:: 対象となる Point
+  ## *return*:: _distance_
+  def distanceToPoint(_point)
+    return distanceInfoToPoint(_point).first ;
+  end
+  
+  #------------------------------------------
+  #++
+  ## 線分までの距離関係情報
+  ## _line_:: 対象となる LineSegment。
+  ## *return*:: [_distance_, _arcPoint_, _linePoint_]。
+  def distanceInfoToLineSegment(_line)
+    (_arcPoint, _linePoint) = self.closestPointPairFromLineSegment(_line)
+    _distance = _arcPoint.distanceToPoint(_linePoint) ;
+
+    return [_distance, _arcPoint, _linePoint] ;
+  end
+
+  #----------------------
+  #++
+  ## 線分までの距離
+  ## _line_:: 対象となる LineSegment。
+  ## *return*:: _distance_
+  def distanceToLineSegment(_line)
+    return distanceInfoToLineSegment(_line).first ;
+  end
+
+  #------------------------------------------
+  #++
+  ## 折れ線までの距離関係情報
+  ## _lineString_:: 対象となる LineString。
+  ## *return*:: [_distance_, _arcPoint_, _linePoint_, _lineSegment_]。
+  def distanceInfoToLineString(_lineString)
+    _minDistance = nil ;
+    _minLine = nil ;
+    _minArcPoint = nil ;
+    _minLinePoint = nil ;
+
+    _lineString.eachLine{|_line|
+      (_distance, _arcPoint, _linePoint) =
+        self.distanceInfoToLineSegment(_line) ;
+      if(_minDistance.nil? || _minDistance > _distance) then
+        _minDistance = _distance ;
+        _minLine = _line ;
+        _minArcPoint = _arcPoint ;
+        _minLinePoint = _linePoint ;
+      end
+    }
+
+    return [_minDistance, _minArcPoint, _minLinePoint, _line] ;
+  end
+
+  #----------------------
+  #++
+  ## 折れ線までの距離
+  ## _lineString_:: 対象となる LineString。
+  ## *return*:: _distance_
+  def distanceToLineString(_lineString)
+    return distanceInfoToLineString(_lineString).first ;
+  end
+
+  #------------------------------------------
+  #++
+  ## 円までの距離関係情報
+  ## _otherCircle_:: 対象となる Ellipse (円である必要）
+  ## *return*:: [_distance_, _selfPoint_, _otherPoint_]。
+  def distanceInfoToEllipse(_otherCircle)
+    (_selfPoint, _otherPoint) =
+      self.closestPointPairFromEllipse(_otherCircle) ;
+    _distance = _selfPoint.distanceToPoint(_otherPoint) ;
+
+    return [_distance, _selfPoint, _otherPoint] ;
+  end
+
+  #----------------------
+  #++
+  ## 円までの距離
+  ## _otherCircle_:: 対象となる Ellipse (円である必要）
+  ## *return*:: _distance_。
+  def distanceToEllipse(_otherCircle)
+    return distanceInfoToEllipse(_otherCircle).first ;
+  end
+  
+  #--////////////////////////////////////////////////////////////
+  ## 出力
   #------------------------------------------
   #++
   ## Ellipse を描画
@@ -220,7 +407,7 @@ if($0 == __FILE__) then
 
     #----------------------------------------------------
     #++
-    ## 
+    ##  new
     def test_a
       circle0 = Ellipse.newCircle([1,1,1], 1.5, [1,1,-1]) ;
       circle1 = Ellipse.newCircle([1,1,1], 2.3, [1,1,-1]) ;
@@ -236,6 +423,64 @@ if($0 == __FILE__) then
       }
       
     end
+
+    #----------------------------------------------------
+    #++
+    ## foot point
+    def test_b
+      center = [1,1,1] ;
+      axisMajor = Vector.new(rand() - 0.5, rand() - 0.5, rand() - 0.5).unit();
+      axisMinor = Vector.new(rand() - 0.5, rand() - 0.5, rand() - 0.5).unit();
+      circle = Ellipse.new(center, axisMajor, axisMinor) ;
+
+      d = 3.0 ;
+      point = Point.new(rand() - 0.5, rand() - 0.5, rand() - 0.5).unit(d);
+
+      foot = circle.footPointFrom(point) ;
+      footLine = LineSegment.new(point, foot) ;
+
+      line = LineSegment.new(Point.new(rand() - 0.5,
+                                       rand() - 0.5,
+                                       rand() - 0.5).unit(d),
+                             Point.new(rand() - 0.5,
+                                       rand() - 0.5,
+                                       rand() - 0.5).unit(d)) ;
+      distPair = circle.closestPointPairFromLineSegment(line) ;
+      distLine = LineSegment.new(*distPair) ;
+
+      center2 = [0,0,0] ;
+      axisMajor2 = Vector.new(rand() - 0.5, rand() - 0.5, rand() - 0.5).unit();
+      axisMinor2 = Vector.new(rand() - 0.5, rand() - 0.5, rand() - 0.5).unit();
+      circle2 = Ellipse.new(center2, axisMajor2, axisMinor2) ;
+
+      distPair2 = circle.closestPointPairFromEllipse(circle2) ;
+      distLine2 = LineSegment.new(*distPair2) ;
+
+      p [:distPoint, circle.distanceInfoToPoint(point)] ;
+      p [:distLine, circle.distanceInfoToLineSegment(line)] ;
+      p [:distCircle, circle.distanceInfoToEllipse(circle2)] ;
+
+      gconf = { xlabel: "X", ylabel: "Y", zlabel: "Z",
+                xrange: [-3,3], yrange: [-3,3], zrange: [-3,3],
+              } ;
+      Gnuplot::directMulti3dPlot([:circle, :point, :foot,
+                                  :line, :dist, :circle2, :dist2],
+                                 gconf){|gplot|
+        circle.draw(gplot, :circle) ;
+        point.draw(gplot, :point) ;
+        footLine.draw(gplot, :foot) ;
+        
+        line.draw(gplot, :line) ;
+        distLine.draw(gplot, :dist) ;
+
+        circle2.draw(gplot, :circle2) ;
+        distLine2.draw(gplot, :dist2) ;
+      }
+      
+    end
+
+    
+    
 
 
   end # class TC_Foo < Test::Unit::TestCase
